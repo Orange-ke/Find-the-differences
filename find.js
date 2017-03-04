52e07919c6db91a3772ac0515d77c55237a4c275
Orange-ke <keyang915@gmail.com>
2017-03-04 12:26:52 +0800

    'rebuilt'

--- find.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#play").on("click",reFn);
-	function reFn() {
-       $("#bar_").addClass("bar");
-       $("#spinner_").addClass("spinner");
-       $("#play").css("background-position","-107px 0px");
-   }
-})
